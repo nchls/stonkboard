6ea9c2fd7ee934ea8ca76fe4010bcd32740f6855
Ian Nicholas <ian@nchls.com>
2021-12-23 19:44:57 -0500

    Removing Finnhub from settings

--- src/settings_example.ts
+++ src/settings_example.ts
@@ -1,7 +1,4 @@
 // Copy this to settings.ts and update values
 
-export const FINNHUB_API_URL = "https://finnhub.io/api/";
-export const FINNHUB_API_KEY = "";
-
 export const ALPHA_VANTAGE_API_URL = "https://www.alphavantage.co/query";
 export const ALPHA_VANTAGE_API_KEY = "";
